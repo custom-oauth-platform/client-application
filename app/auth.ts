ffb908e38de47c29e93a90a77b63ebd05793d676
hyeyoon23 <109073947+hyeyoon23@users.noreply.github.com>
2026-03-13 14:04:03 +0900

    Initial Commit - client application

---
+++ app/auth.ts
@@ -0,0 +1,40 @@
+import NextAuth from "next-auth";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  debug: true,
+  trustHost: true,
+  session: { strategy: "jwt" },
+  providers: [
+    {
+      id: "custom-oauth",
+      name: "Custom OAuth",
+      type: "oidc",
+
+      issuer: "http://localhost:9000",
+
+      authorization: {
+        url: "http://localhost:9000/oauth2/authorize",
+        params: {
+          scope: "openid profile read write",
+        },
+      },
+
+      token: "http://localhost:9000/oauth2/token",
+      userinfo: "http://localhost:9000/userinfo",
+
+      clientId: "oauth2-client-app",
+      clientSecret: "secret",
+
+      checks: ["pkce", "state"],
+
+      profile(profile) {
+        return {
+          id: String(profile.sub),
+          name: profile.name ?? "User",
+          email: profile.email ?? null,
+          image: profile.picture ?? null,
+        };
+      },
+    },
+  ],
+});
